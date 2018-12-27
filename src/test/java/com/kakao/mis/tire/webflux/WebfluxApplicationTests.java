fcfd2c3b26e7e823563187924264cc0297eb1cd3
hotire <gngh0101@gmail.com>
2018-12-28 00:14:03 +0900

    init

---
+++ src/test/java/com/kakao/mis/tire/webflux/WebfluxApplicationTests.java
@@ -0,0 +1,30 @@
+package com.kakao.mis.tire.webflux;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class WebfluxApplicationTests {
+
+  @Autowired
+  private WebTestClient webTestClient;
+
+  @Test
+  public void get() {
+    webTestClient.get().uri("/hello").accept(MediaType.TEXT_PLAIN).exchange().expectStatus().isOk().expectBody().consumeWith(System.out::print);
+  }
+
+}
+
---
+++ src/test/java/com/kakao/mis/tire/webflux/WebfluxApplicationTestsMock.java
@@ -0,0 +1,24 @@
+package com.kakao.mis.tire.webflux;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureWebTestClient
+public class WebfluxApplicationTestsMock {
+
+  @Autowired
+  private WebTestClient webTestClient;
+
+  @Test
+  public void get() {
+    webTestClient.get().uri("/hello").accept(MediaType.TEXT_PLAIN).exchange().expectStatus().isOk().expectBody(String.class).isEqualTo("Hello, Spring!").consumeWith(System.out::print);
+  }
+}
