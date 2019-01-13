991e2c6e14e19151cfd6a60c8983a0e5c5226c6d
hotire <gngh0101@gmail.com>
2019-01-13 16:50:20 +0900

    Learn how to create Flux instances

---
+++ src/main/java/com/kakao/mis/tire/webflux/example/Part01Flux.java
@@ -0,0 +1,36 @@
+package com.kakao.mis.tire.webflux.example;
+
+
+import java.time.Duration;
+import java.util.Arrays;
+import reactor.core.publisher.Flux;
+
+/**
+ * Learn how to create Flux instances.
+ *
+ * @author Sebastien Deleuze
+ * @see <a href="http://projectreactor.io/docs/core/release/api/reactor/core/publisher/Flux.html">Flux Javadoc</a>
+ */
+public class Part01Flux {
+
+  Flux<String> emptyFlux() {
+    return Flux.empty();
+  }
+
+  Flux<String> fooBarFluxFromValues() {
+    return Flux.just("foo","bar");
+  }
+
+  Flux<String> fooBarFluxFromList() {
+    return Flux.fromIterable(Arrays.asList("foo","bar"));
+  }
+
+  Flux<String> errorFlux() {
+    return Flux.error(new IllegalStateException());
+  }
+
+  Flux<Long> counter() {
+    return Flux.interval(Duration.ofMillis(100)).take(10);
+  }
+
+}
