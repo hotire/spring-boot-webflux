072b168872ae4a5e5db6a17ed7a9d380cdb12c5e
hotire <gngh0101@gmail.com>
2019-01-19 13:24:11 +0900

    Learn how to create Mono instances.

---
+++ src/main/java/com/kakao/mis/tire/webflux/example/Part02Mono.java
@@ -0,0 +1,28 @@
+package com.kakao.mis.tire.webflux.example;
+
+import reactor.core.publisher.Mono;
+
+/**
+ * Learn how to create Mono instances.
+ *
+ * @author Sebastien Deleuze
+ * @see <a href="http://projectreactor.io/docs/core/release/api/reactor/core/publisher/Mono.html">Mono Javadoc</a>
+ */
+
+public class Part02Mono {
+  Mono<String> emptyMono() {
+    return Mono.empty();
+  }
+
+  Mono<String> monoWithNoSignal() {
+    return Mono.never();
+  }
+
+  Mono<String> fooMono() {
+    return Mono.just("foo");
+  }
+
+  Mono<String> errorMono() {
+    return Mono.error(new IllegalStateException());
+  }
+}
