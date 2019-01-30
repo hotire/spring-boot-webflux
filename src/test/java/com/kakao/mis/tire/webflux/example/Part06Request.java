0c2a2b26c3a566e28fd0d0ff96bed2ad5ec211f7
hotire <gngh0101@gmail.com>
2019-01-30 23:23:50 +0900

    Learn how to control the demand

---
+++ src/test/java/com/kakao/mis/tire/webflux/example/Part06Request.java
@@ -0,0 +1,38 @@
+package com.kakao.mis.tire.webflux.example;
+
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+/**
+ *
+ * https://tech.io/playgrounds/929/reactive-programming-with-reactor-3/Request
+ * Learn how to control the demand.
+ *
+ * @author Sebastien Deleuze
+ */
+public class Part06Request {
+
+  private ReactiveRepository<User> repository = new ReactiveUserRepository<>();
+
+  StepVerifier requestAllExpectFour(Flux<User> flux) {
+    return StepVerifier.create(flux).expectNextCount(4).expectComplete();
+  }
+
+  StepVerifier requestOneExpectSkylerThenRequestOneExpectJesse(Flux<User> flux) {
+    return StepVerifier.create(flux)
+      .thenRequest(1).expectNext(User.SKYLER)
+      .thenRequest(1).expectNext(User.JESSE).thenCancel();
+  }
+
+  Flux<User> fluxWithLog() {
+    return repository.findAll().log();
+  }
+
+  Flux<User> fluxWithDoOnPrintln() {
+    return repository.findAll().doOnSubscribe(subscription -> System.out.println("Starring"))
+      .doOnNext(user -> {
+        System.out.println(user.getFirstname() + " " + user.getLastname());
+      })
+      .doOnComplete(() -> System.out.println("The end!"));
+  }
+}
