dff82db19b52986de92f55016c96b8b14f4916e2
hotire <gngh0101@gmail.com>
2021-06-26 12:05:21 +0900

    Polish

--- src/main/java/com/kakao/mis/tire/webflux/websocket/server/ServerHandler.java
+++ src/main/java/com/kakao/mis/tire/webflux/websocket/server/ServerHandler.java
@@ -11,17 +11,15 @@ import org.springframework.web.reactive.socket.WebSocketSession;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.publisher.MonoSink;
 
 @Slf4j
 @Component
 public class ServerHandler implements WebSocketHandler {
 
-    private final Map<String, MonoSink<String>> map = new ConcurrentHashMap<>();
-    private final Map<String, WebSocketSession> map2 = new ConcurrentHashMap<>();
+    private final Map<String, WebSocketSession> sessionMapById = new ConcurrentHashMap<>();
 
-    public void send(final String message) {
-        map2.values().forEach(session -> {
+    public void broadcast(final String message) {
+        sessionMapById.values().forEach(session -> {
             session.send(Mono.fromCallable(() -> session.textMessage(message)))
                    .doFinally(s -> log.info("Server -> sent: [{}] to client id=[{}]", message, session.getId()))
                    .subscribe();
@@ -32,22 +30,20 @@ public class ServerHandler implements WebSocketHandler {
     public Mono<Void> handle(final WebSocketSession session) {
         session.getAttributes();
         return session.receive()
-                      .doOnNext(message -> {
-                          log.info("Server -> client connected id=[{}]", session.getId());
-                        })
+                      .doOnNext(message -> log.info("Server -> client connected id=[{}]", session.getId()))
                       .map(WebSocketMessage::getPayloadAsText)
                       .doOnNext(message -> log.info("Server -> received from client id=[{}]: [{}]", session.getId(), message))
                       .flatMap(message -> send(session))
                       .doFinally(signalType -> {
                           log.info("Terminating WebSocket Session (client side) sig: [{}], [{}]", signalType.name(), session.getId());
                           session.close();
-                          map2.remove(session.getId());
+                          sessionMapById.remove(session.getId());
                       })
                       .then();
     }
 
     private Flux<Void> send(final WebSocketSession session) {
-        map2.put(session.getId(), session);
+        sessionMapById.put(session.getId(), session);
         return Flux.empty();
 //        return Flux.interval(Duration.ofMinutes(1))
 //                   .map(value -> Long.toString(value))
