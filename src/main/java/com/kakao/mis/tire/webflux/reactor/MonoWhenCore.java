42e07fba13b0bc1b116111ce4df607725a4f83d7
hotire <gngh0101@gmail.com>
2022-03-02 00:10:44 +0900

    Learn mono-when

--- src/main/java/com/kakao/mis/tire/webflux/reactor/MonoWhenCore.java
+++ src/main/java/com/kakao/mis/tire/webflux/reactor/MonoWhenCore.java
@@ -1,7 +1,18 @@
 package com.kakao.mis.tire.webflux.reactor;
 
+import org.reactivestreams.Publisher;
+
+import reactor.core.publisher.Mono;
+
 /**
  * @see reactor.core.publisher.MonoWhen
  */
 public class MonoWhenCore {
+    
+    /**
+     * @see reactor.core.publisher.Mono#when(Publisher[])
+     */
+    public static Mono<Void> when(Publisher<?>... sources) {
+        return Mono.when(sources);
+    }
 }
