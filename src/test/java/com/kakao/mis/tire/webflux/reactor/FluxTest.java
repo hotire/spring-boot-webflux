2abdcbf2724a9f3d7b9a968f9744e70c6a8793b3
hotire <gngh0101@gmail.com>
2022-05-11 00:29:39 +0900

    Verify share in flux

---
+++ src/test/java/com/kakao/mis/tire/webflux/reactor/FluxTest.java
@@ -0,0 +1,45 @@
+package com.kakao.mis.tire.webflux.reactor;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+@Slf4j
+class FluxTest {
+
+    @Test
+    void share() throws InterruptedException {
+        final Flux<String> coldSource  = Flux.interval(Duration.ofMillis(1))
+                                              .map(Object::toString)
+                                              .take(10)
+                                              .doOnSubscribe(s -> log.info("doOnSubscribe"));
+
+        final Flux<String> hot = coldSource.share();
+        hot.subscribe(log::info);
+        hot.subscribe(log::info);
+        Thread.sleep(3);
+        hot.subscribe(log::info);
+        hot.subscribe(log::info);
+    }
+    @Test
+    void shareMulti() throws InterruptedException {
+        final Flux<String> coldSource  = Flux.interval(Duration.ofMillis(1))
+                                             .map(Object::toString)
+                                             .take(10)
+                                             .subscribeOn(Schedulers.boundedElastic())
+                                             .doOnSubscribe(s -> log.info("doOnSubscribe"));
+
+        final Flux<String> hot = coldSource.share();
+        hot.subscribe(log::info);
+        hot.subscribe(log::info);
+        Thread.sleep(3);
+        hot.subscribe(log::info);
+        hot.subscribe(log::info);
+
+        Thread.sleep(1000L);
+    }
+}
