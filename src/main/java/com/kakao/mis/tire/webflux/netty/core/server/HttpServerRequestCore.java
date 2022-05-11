b1cfeb4870a8d05dc252852f10a5177baa700b3d
hotire <gngh0101@gmail.com>
2022-05-12 00:01:59 +0900

    Learn HttpServerRequestCore

---
+++ src/main/java/com/kakao/mis/tire/webflux/netty/core/server/HttpServerRequestCore.java
@@ -0,0 +1,12 @@
+package com.kakao.mis.tire.webflux.netty.core.server;
+
+import org.springframework.http.server.reactive.AbstractServerHttpRequest;
+
+/**
+ * @see AbstractServerHttpRequest#getNativeRequest()
+ * @see reactor.netty.http.server.HttpServerRequest
+ * @see reactor.netty.http.HttpOperations
+ * @see reactor.netty.http.server.HttpServerOperations
+ */
+public class HttpServerRequestCore {
+}
