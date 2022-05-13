cf077d87eae727980185c709e4b3b7000d5b31ea
hotire <gngh0101@gmail.com>
2022-05-13 09:49:16 +0900

    Learn NettyWebServerCore

---
+++ src/main/java/com/kakao/mis/tire/webflux/netty/core/server/NettyWebServerCore.java
@@ -0,0 +1,18 @@
+package com.kakao.mis.tire.webflux.netty.core.server;
+
+import org.springframework.boot.web.embedded.netty.NettyWebServer;
+import org.springframework.boot.web.server.WebServerException;
+
+/**
+ * @see org.springframework.boot.web.embedded.netty.NettyWebServer
+ */
+public class NettyWebServerCore {
+
+    /**
+     * @see NettyWebServer#start()
+     */
+    public void start() throws WebServerException {
+
+    }
+
+}
