a63ef1894e721b8372491cbcc96a02d518b351c1
hotire <gngh0101@gmail.com>
2022-05-14 11:49:37 +0900

    Add DataBufferCore

---
+++ src/main/java/com/kakao/mis/tire/webflux/io/buffer/DataBufferCore.java
@@ -0,0 +1,17 @@
+package com.kakao.mis.tire.webflux.io.buffer;
+
+import org.springframework.core.io.buffer.DataBuffer;
+
+/**
+ * @see DataBuffer
+ */
+public class DataBufferCore {
+
+    /**
+     * @see DataBuffer#readableByteCount()
+     */
+    public int readableByteCount() {
+        return 0;
+    }
+
+}
