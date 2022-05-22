a45d6fae4fb8d562521370c202c0b20d9711065a
hotire <gngh0101@gmail.com>
2022-05-23 00:54:28 +0900

    Add DisposableCore

---
+++ src/main/java/com/kakao/mis/tire/webflux/reactor/DisposableCore.java
@@ -0,0 +1,11 @@
+package com.kakao.mis.tire.webflux.reactor;
+
+/**
+ * Indicates that a task or resource can be cancelled/disposed.
+ * 작업 또는 리소스가 취소/폐기될 수 있음을 나타냅니다.
+ *
+ * @see reactor.core.Disposable
+ *
+ */
+public class DisposableCore {
+}
