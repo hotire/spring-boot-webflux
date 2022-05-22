7a1d50e5dfb3d88a1a45d3397d4b6169d29ea858
hotire <gngh0101@gmail.com>
2022-05-22 20:57:13 +0900

    Add DisposableCore

---
+++ src/main/java/com/kakao/mis/tire/webflux/netty/core/channel/DisposableChannelCore.java
@@ -0,0 +1,17 @@
+package com.kakao.mis.tire.webflux.netty.core.channel;
+
+import io.netty.channel.Channel;
+import reactor.netty.DisposableChannel;
+
+/**
+ * @see reactor.netty.DisposableChannel
+ */
+public class DisposableChannelCore {
+
+    /**
+     * @see DisposableChannel#channel() 
+     */
+    public Channel channel() {
+        return null;
+    }
+}
