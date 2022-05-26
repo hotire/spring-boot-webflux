534f18677e5553de6e288e6cf36a8e940a8aa1c5
hotire <gngh0101@gmail.com>
2022-05-27 01:02:44 +0900

    Add ConnectableFluxCore

---
+++ src/main/java/com/kakao/mis/tire/webflux/reactor/flux/ConnectableFluxCore.java
@@ -0,0 +1,17 @@
+package com.kakao.mis.tire.webflux.reactor.flux;
+
+import reactor.core.Disposable;
+import reactor.core.publisher.ConnectableFlux;
+
+/**
+ * @see ConnectableFlux
+ */
+public class ConnectableFluxCore {
+
+    /**
+     * @see ConnectableFlux#connect()
+     */
+    public final Disposable connect() {
+        return null;
+    }
+}
