3296305e2b7f0472516d7824aeb149608cbd4135
hotire <gngh0101@gmail.com>
2022-07-14 00:36:01 +0900

    Add ChannelCore

---
+++ src/main/java/com/kakao/mis/tire/webflux/netty/core/channel/ChannelCore.java
@@ -0,0 +1,17 @@
+package com.kakao.mis.tire.webflux.netty.core.channel;
+
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoop;
+
+/**
+ * @see io.netty.channel.Channel
+ */
+public class ChannelCore {
+
+    /**
+     * @see Channel#eventLoop()
+     */
+    public EventLoop eventLoop() {
+        return null;
+    }
+}
