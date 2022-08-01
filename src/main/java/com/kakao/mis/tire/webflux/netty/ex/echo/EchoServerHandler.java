3a8c37cb0ff45feb9683fac476b7d1bec83ee3f0
hotire <gngh0101@gmail.com>
2022-08-01 16:39:34 +0900

    Add EchoServerHandler

---
+++ src/main/java/com/kakao/mis/tire/webflux/netty/ex/echo/EchoServerHandler.java
@@ -0,0 +1,36 @@
+package com.kakao.mis.tire.webflux.netty.ex.echo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
+
+@Slf4j
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (msg instanceof ByteBuf) {
+            final ByteBuf in = (ByteBuf) msg;
+            log.info("Server received : {} ", in.toString(Charset.defaultCharset()));
+            ctx.write(in);
+            return;
+        }
+        super.channelRead(ctx, msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        log.error(cause.getMessage(), cause);
+        ctx.close();
+    }
+}
