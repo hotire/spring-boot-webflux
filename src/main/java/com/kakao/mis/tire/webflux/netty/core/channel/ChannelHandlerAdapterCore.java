509b4663e6eb10da7948ef067d302ff482c5bd73
hotire <gngh0101@gmail.com>
2022-08-02 18:46:40 +0900

    Add ChannelHandlerAdapterCore

---
+++ src/main/java/com/kakao/mis/tire/webflux/netty/core/channel/ChannelHandlerAdapterCore.java
@@ -0,0 +1,9 @@
+package com.kakao.mis.tire.webflux.netty.core.channel;
+
+import io.netty.channel.ChannelHandlerAdapter;
+
+/**
+ * @see ChannelHandlerAdapter
+ */
+public class ChannelHandlerAdapterCore {
+}
