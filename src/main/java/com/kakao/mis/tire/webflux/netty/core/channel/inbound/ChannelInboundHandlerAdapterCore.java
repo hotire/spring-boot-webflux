5c1366c19263a137b415bbef5d440c95d280b899
hotire <gngh0101@gmail.com>
2022-08-03 00:17:50 +0900

    Add ChannelInboundHandlerAdapter

---
+++ src/main/java/com/kakao/mis/tire/webflux/netty/core/channel/inbound/ChannelInboundHandlerAdapterCore.java
@@ -0,0 +1,9 @@
+package com.kakao.mis.tire.webflux.netty.core.channel.inbound;
+
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @see ChannelInboundHandlerAdapter
+ */
+public class ChannelInboundHandlerAdapterCore {
+}
