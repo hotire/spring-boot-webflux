387a1ffbfc4211f1a2383c10465fb0ab7aa51737
hotire <gngh0101@gmail.com>
2022-08-05 14:13:58 +0900

    Add HttpServerCodecCore

---
+++ src/main/java/com/kakao/mis/tire/webflux/netty/core/handler/codec/http/HttpServerCodecCore.java
@@ -0,0 +1,9 @@
+package com.kakao.mis.tire.webflux.netty.core.handler.codec.http;
+
+import io.netty.handler.codec.http.HttpServerCodec;
+
+/**
+ * @see HttpServerCodec
+ */
+public class HttpServerCodecCore {
+}
