3b52b699b77252b76f65d59f899728e55e84c029
hotire <gngh0101@gmail.com>
2022-08-05 16:47:53 +0900

    Learn epoll

---
+++ src/main/java/com/kakao/mis/tire/webflux/netty/core/channel/epoll/AbstractEpollChannelCore.java
@@ -0,0 +1,12 @@
+package com.kakao.mis.tire.webflux.netty.core.channel.epoll;
+
+import io.netty.channel.epoll.AbstractEpollServerChannel;
+import io.netty.channel.epoll.EpollServerSocketChannel;
+
+/**
+ * @see io.netty.channel.epoll.AbstractEpollChannel
+ * @see AbstractEpollServerChannel
+ * @see EpollServerSocketChannel
+ */
+public class AbstractEpollChannelCore {
+}
---
+++ src/main/java/com/kakao/mis/tire/webflux/netty/core/channel/epoll/EpollEventLoopGroupCore.java
@@ -0,0 +1,9 @@
+package com.kakao.mis.tire.webflux.netty.core.channel.epoll;
+
+import io.netty.channel.epoll.EpollEventLoopGroup;
+
+/**
+ * @see EpollEventLoopGroup
+ */
+public class EpollEventLoopGroupCore {
+}
