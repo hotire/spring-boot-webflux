4dc8fc73854dcb103ed8175a4114b39a962050ba
hotire <gngh0101@gmail.com>
2022-08-06 12:39:33 +0900

    Add ChannelInitializerCore

---
+++ src/main/java/com/kakao/mis/tire/webflux/netty/core/channel/inbound/ChannelInitializerCore.java
@@ -0,0 +1,17 @@
+package com.kakao.mis.tire.webflux.netty.core.channel.inbound;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+
+/**
+ * @see ChannelInitializer
+ */
+public class ChannelInitializerCore {
+
+    /**
+     * @see ChannelInitializer#initChannel(ChannelHandlerContext) 
+     */
+    private boolean initChannel(ChannelHandlerContext ctx) throws Exception {
+        return false;
+    }
+}
