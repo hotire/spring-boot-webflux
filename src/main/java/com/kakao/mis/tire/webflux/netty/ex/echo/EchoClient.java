755e2e0426152e8715e5d3e04f7524a74a1e48fe
hotire <gngh0101@gmail.com>
2022-08-07 13:13:50 +0900

    Add EchoClient

---
+++ src/main/java/com/kakao/mis/tire/webflux/netty/ex/echo/EchoClient.java
@@ -0,0 +1,13 @@
+package com.kakao.mis.tire.webflux.netty.ex.echo;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class EchoClient {
+    private final String host;
+    private final int port;
+
+    public void start() {
+
+    }
+}
