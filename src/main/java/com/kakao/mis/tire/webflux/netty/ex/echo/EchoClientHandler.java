6a3fb956a8391a68b48d796b2ff143387e968de4
hotire <gngh0101@gmail.com>
2022-08-07 13:13:31 +0900

    Add EchoClientHandler

---
+++ src/main/java/com/kakao/mis/tire/webflux/netty/ex/echo/EchoClientHandler.java
@@ -0,0 +1,31 @@
+package com.kakao.mis.tire.webflux.netty.ex.echo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
+
+@ChannelHandler.Sharable
+@Slf4j
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Hello Netty", Charset.defaultCharset()));
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+        log.info("Client received : {}", msg.toString(Charset.defaultCharset()));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        log.error(cause.getMessage(), cause);
+        ctx.close();
+    }
+}
