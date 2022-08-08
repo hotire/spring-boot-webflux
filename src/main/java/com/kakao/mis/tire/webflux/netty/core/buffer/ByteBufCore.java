777f6120932868858123866613812942fca539b3
hotire <gngh0101@gmail.com>
2022-08-08 19:11:13 +0900

    Add ByteBufCore

---
+++ src/main/java/com/kakao/mis/tire/webflux/netty/core/buffer/ByteBufCore.java
@@ -0,0 +1,13 @@
+package com.kakao.mis.tire.webflux.netty.core.buffer;
+
+import io.netty.buffer.AbstractByteBuf;
+import io.netty.buffer.ByteBuf;
+
+/**
+ * @see ByteBuf;
+ * @see AbstractByteBuf;
+ */
+public class ByteBufCore {
+    int readerIndex;
+    int writerIndex;
+}
