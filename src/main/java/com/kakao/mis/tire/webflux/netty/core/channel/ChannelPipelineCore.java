61176308941a28aeba485ae72e8d07eb20e0cf5f
hotire <gngh0101@gmail.com>
2022-08-08 19:25:27 +0900

    Update ChannelPipelineCore

--- src/main/java/com/kakao/mis/tire/webflux/netty/core/channel/ChannelPipelineCore.java
+++ src/main/java/com/kakao/mis/tire/webflux/netty/core/channel/ChannelPipelineCore.java
@@ -23,15 +23,25 @@ public class ChannelPipelineCore {
     /**
      * @see ChannelPipeline#addLast(EventExecutorGroup, String, ChannelHandler)
      * @see DefaultChannelPipeline#addLast(EventExecutorGroup, String, ChannelHandler)
+     *
+     * blocking 인 경우 EventExecutorGroup을 함께 넘긴다.
      */
     public final ChannelPipeline addLast(EventExecutorGroup group, String name, ChannelHandler handler) {
         return null;
     }
-
+    
     /**
      * @see DefaultChannelPipeline#newContext(EventExecutorGroup, String, ChannelHandler)
      */
     private ChannelHandlerContext newContext(EventExecutorGroup group, String name, ChannelHandler handler) {
+        return null;
+    }
+
+    /**
+     * @see ChannelPipeline#addLast(String, ChannelHandler)
+     * @see DefaultChannelPipeline#addLast(String, ChannelHandler)
+     */
+    public final ChannelPipeline addList(String name, ChannelHandler handler) {
         return null;
     }
 }
