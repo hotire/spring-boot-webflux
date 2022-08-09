8aec4a51892dbb8da4864d3ee86a5c1b7c515da0
hotire <gngh0101@gmail.com>
2022-08-09 15:13:07 +0900

    Add ByteToMessageCodecCore

---
+++ src/main/java/com/kakao/mis/tire/webflux/netty/core/handler/codec/ByteToMessageCodecCore.java
@@ -0,0 +1,9 @@
+package com.kakao.mis.tire.webflux.netty.core.handler.codec;
+
+import io.netty.handler.codec.ByteToMessageCodec;
+
+/**
+ * @see ByteToMessageCodec
+ */
+public class ByteToMessageCodecCore {
+}
