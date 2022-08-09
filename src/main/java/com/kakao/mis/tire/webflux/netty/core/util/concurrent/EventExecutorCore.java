264344a7295600da9ab8a4c0e8cb3147b4be3889
hotire <gngh0101@gmail.com>
2022-08-09 15:14:37 +0900

    Add EventExecutorCore

---
+++ src/main/java/com/kakao/mis/tire/webflux/netty/core/util/concurrent/EventExecutorCore.java
@@ -0,0 +1,9 @@
+package com.kakao.mis.tire.webflux.netty.core.util.concurrent;
+
+import io.netty.util.concurrent.EventExecutor;
+
+/**
+ * @see EventExecutor
+ */
+public class EventExecutorCore {
+}
