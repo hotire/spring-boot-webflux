98efbcac85d074bbdd9923c9037a679ba6063af1
hotire <gngh0101@gmail.com>
2022-08-10 11:17:50 +0900

    Add NioServerSocketChannelCore

---
+++ src/main/java/com/kakao/mis/tire/webflux/netty/core/channel/socket/nio/NioServerSocketChannelCore.java
@@ -0,0 +1,9 @@
+package com.kakao.mis.tire.webflux.netty.core.channel.socket.nio;
+
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @see NioServerSocketChannel
+ */
+public class NioServerSocketChannelCore {
+}
