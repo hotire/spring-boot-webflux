27c762c489a928f6f63253585b9070aa45b26621
hotire <gngh0101@gmail.com>
2022-08-17 10:23:26 +0700

    EventExecutorGroupCore

---
+++ src/main/java/com/kakao/mis/tire/webflux/netty/core/util/concurrent/EventExecutorGroupCore.java
@@ -0,0 +1,9 @@
+package com.kakao.mis.tire.webflux.netty.core.util.concurrent;
+
+import io.netty.util.concurrent.EventExecutorGroup;
+
+/**
+ * @see EventExecutorGroup
+ */
+public class EventExecutorGroupCore {
+}
