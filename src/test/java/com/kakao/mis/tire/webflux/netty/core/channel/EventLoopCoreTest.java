ee25a6e455c26b01d199d294eb59a7dbfa83ebff
hotire <gngh0101@gmail.com>
2022-08-21 21:49:59 +0900

    Update schedule

--- src/test/java/com/kakao/mis/tire/webflux/netty/core/channel/EventLoopCoreTest.java
+++ src/test/java/com/kakao/mis/tire/webflux/netty/core/channel/EventLoopCoreTest.java
@@ -3,23 +3,23 @@ package com.kakao.mis.tire.webflux.netty.core.channel;
 import io.netty.channel.DefaultEventLoop;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class EventLoopCoreTest {
 
     @Test
-    void schedule() throws InterruptedException {
+    void schedule() throws InterruptedException, ExecutionException {
         // given
         final DefaultEventLoop defaultEventLoop = new DefaultEventLoop();
 
         // when
-        defaultEventLoop.schedule(() -> {
+        final Future<?> future = defaultEventLoop.schedule(() -> {
             System.out.println("hello");
         }, 1, TimeUnit.SECONDS);
 
         // no assert
-        Thread.sleep(1000L);
+        future.get();
     }
 }
